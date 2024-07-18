83e17f6cc3418ea3dbbfb91c608a5dc32d2725e9
RamiroPinchao <antonio.pinchao@gmail.com>
2024-07-18 17:58:32 -0500

    posts inclomplete

--- src/modules/posts/services/posts.service.ts
+++ src/modules/posts/services/posts.service.ts
@@ -1,1 +1,27 @@
-// 
+// here i need to create the posts service
+
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreatePostDto } from '../dto/create-post.dto';
+import { PostEntity } from '../entities/post.entity';
+
+@Injectable()
+export class PostsService { 
+    constructor(
+        @InjectRepository(PostEntity)
+        private readonly postRepository: Repository<PostEntity>
+    ) {}
+
+
+    // here i need to create a post
+    createPost(post: CreatePostDto) {
+    }
+
+
+    // here i need to get all posts
+    getPosts() {
+    }
+
+    
+}

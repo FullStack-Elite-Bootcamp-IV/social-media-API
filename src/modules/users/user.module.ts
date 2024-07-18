fcd52835784c68949165f038d79d9d2669df6bf0
RamiroPinchao <antonio.pinchao@gmail.com>
2024-07-18 17:55:20 -0500

    delete some things

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,20 +1,16 @@
 // user module 
-
-import { Module } from '@nestjs/common';
 import { UserService } from '../users/services/user.service';
 import { UsersController } from '../users/controllers/users.controller';
 import { UserEntity } from '../users/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserDto } from '../users/dto/create-user.dto';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
-import { PassportModule } from '@nestjs/passport';
-import { JwtService } from '@nestjs/jwt';   
+import { JwtService } from '@nestjs/jwt'; 
+import { Module } from '@nestjs/common';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([UserEntity]),
-        PassportModule.register({ defaultStrategy: 'jwt' }),
         JwtModule.register({}),
         ConfigModule
     ],

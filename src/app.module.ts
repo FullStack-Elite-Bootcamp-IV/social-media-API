1f36fb9df31f0d6220e92c86000913a586c6aaaf
MateoMor <mateomorales5558@gmail.com>
2024-07-18 22:14:34 -0500

    typeOrm configurado. Para correr migraciones usar los comandos npm run migration:run seguido de npm run migration:generate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
-// make tehe app
 import { Module } from '@nestjs/common';
-@Module({
-  imports: [],
-  controllers: [],
-  providers: [],
-})  
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../.env',
+    }),
+    ConfigModule.forRoot({
+      validationSchema: Joi?.object({
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION_TIME: Joi.string().required(),
+        POSTGRES_HOST: Joi?.string().required(),
+        POSTGRES_PORT: Joi?.number().required(),
+        POSTGRES_USER: Joi?.string().required(),
+        POSTGRES_PASSWORD: Joi?.string().required(),
+        POSTGRES_DB: Joi?.string().required(),
+        PORT: Joi?.number(),
+      })
+    }),
+  ],
+})
 export class AppModule {}

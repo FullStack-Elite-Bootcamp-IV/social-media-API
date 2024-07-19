9cafead47c2bb4fde5c8851acfe657ad03507c50
AGBR121 <angel.burbano@utp.edu.co>
2024-07-19 09:03:50 -0500

    update DTO Favourites

--- src/modules/favourites/dto/create-favourite.dto.ts
+++ src/modules/favourites/dto/create-favourite.dto.ts
@@ -1,18 +1,14 @@
-
-
 import { IsDate, IsNotEmpty, IsString } from 'class-validator';
-import { UserEntity } from 'src/modules/users/entities/user.entity';
-import { PostEntity } from 'src/modules/posts/entities/post.entity';
 
 export class FavouritesDto  {
 
 @IsString()
 @IsNotEmpty()
-userId: UserEntity;
+userId: string;
 
 @IsString()
 @IsNotEmpty()
-postId: PostEntity;
+postId: string;
 
 @IsDate()
 @IsNotEmpty()

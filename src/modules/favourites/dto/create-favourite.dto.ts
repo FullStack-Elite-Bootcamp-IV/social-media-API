5ef24fccf48e59c30ab9f675f25b7399694bf92b
AGBR121 <angel.burbano@utp.edu.co>
2024-07-19 08:59:26 -0500

    dto Favourites

--- src/modules/favourites/dto/create-favourite.dto.ts
+++ src/modules/favourites/dto/create-favourite.dto.ts
@@ -1,7 +1,20 @@
 
-import { PartialType } from '@nestjs/mapped-types';
-import { FavouritesEntity } from '../entities/favourites.entity';
 
-export class FavouritesDto extends PartialType(FavouritesEntity) {
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+import { UserEntity } from 'src/modules/users/entities/user.entity';
+import { PostEntity } from 'src/modules/posts/entities/post.entity';
 
+export class FavouritesDto  {
+
+@IsString()
+@IsNotEmpty()
+userId: UserEntity;
+
+@IsString()
+@IsNotEmpty()
+postId: PostEntity;
+
+@IsDate()
+@IsNotEmpty()
+creationDate: Date;
 }

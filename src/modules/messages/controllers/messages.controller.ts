901740f9b7168e865cfa92ff80998c09c4fc8ef6
pertinaz <manuel.agudelo1@utp.edu.co>
2024-07-19 17:04:39 -0500

    fix the message controller

--- src/modules/messages/controllers/messages.controller.ts
+++ src/modules/messages/controllers/messages.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
 import { MessagesService } from '../services/messages.service';
 import { CreateMessageDto } from '../dto/create-message.dto';
 import { MessageEntity } from '../entities/message.entity';
@@ -7,18 +15,25 @@ import { MessageEntity } from '../entities/message.entity';
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
 
-  // @Post()
-  // createMessage(@Body() createMessageDto: CreateMessageDto): Promise<MessageEntity> { ... }
+  @Post()
+  createMessage(
+    @Body() createMessageDto: CreateMessageDto,
+  ): Promise<MessageEntity> {
+    return this.messagesService.createMessage(createMessageDto);
+  }
 
-  // @Get()
-  // findAllMessages(): Promise<MessageEntity[]> { ... }
+  @Get(':chatId')
+  findMessagesByChat(@Param('chatId') chatId: string): Promise<void> {
+    return this.messagesService.findMessagesByChat(chatId);
+  }
 
-  // @Delete(':id')
-  // deleteMessage(@Param('id') messageId: string): Promise<void> { ... }
+  @Delete(':id')
+  deleteMessage(@Param('id') messageId: string): Promise<void> {
+    return this.messagesService.deleteMessage(messageId);
+  }
 
-  // @Get('chat/:chatId')
-  // findMessagesByChat(@Param('chatId') chatId: string): Promise<MessageEntity[]> { ... }
-
-  // @Get('user/:userId')
-  // findMessagesByUser(@Param('userId') userId: string): Promise<MessageEntity[]> { ... }
+  @Get(':userId')
+  async findMessagesByUser(@Param('userId') userId: string): Promise<void> {
+    return this.messagesService.findMessagesByUser(userId);
+  }
 }

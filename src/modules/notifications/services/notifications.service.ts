261e745a52bb3c8a284d22400a8196c054f79439
pertinaz <manuel.agudelo1@utp.edu.co>
2024-07-19 14:46:35 -0500

    refactor notifications service

--- src/modules/notifications/services/notifications.service.ts
+++ src/modules/notifications/services/notifications.service.ts
@@ -10,56 +10,55 @@ import { UserEntity } from 'src/modules/users/entities/user.entity';
 export class NotificationsService {
   constructor(
     @InjectRepository(NotificationEntity)
-    private notificationRepository: Repository<NotificationEntity>
+    private notificationRepository: Repository<NotificationEntity>,
   ) {}
 
   // Function to create a new notification
-  async createNotification(createNotificationDto: CreateNotificationDto): Promise<NotificationEntity> {
-    const {emisorUser,receptorUser, status, action,title,description} = createNotificationDto
-    try{
-      if(!emisorUser || !receptorUser){
+  async createNotification(
+    createNotificationDto: CreateNotificationDto,
+  ): Promise<NotificationEntity> {
+    const { emisorUser, receptorUser, status, action, title, description } =
+      createNotificationDto;
+    try {
+      if (!emisorUser || !receptorUser) {
         throw new Error('Emisor or receptor user invalid');
       }
-      const notification = this.notificationRepository.create(
-        {
-          emisorUser,
-          receptorUser,
-          status,
-          action,
-          title,
-          description,
-        }
-      );
+      const notification = this.notificationRepository.create({
+        emisorUser,
+        receptorUser,
+        status,
+        action,
+        title,
+        description,
+      });
       await this.notificationRepository.save(notification);
       return notification;
-    }catch(err){
+    } catch (err) {
       throw new Error(err);
     }
   }
 
   // Function to delete a notification by ID
-  async deleteNotification(notificationId: string): Promise<void> { 
-    try{
-      if(!notificationId){
+  async deleteNotification(notificationId: string): Promise<void> {
+    try {
+      if (!notificationId) {
         throw new Error('Notification inexistent');
       }
       await this.notificationRepository.delete(notificationId);
-    }
-    catch(err){
+    } catch (err) {
       throw new Error(err);
     }
-   }
+  }
 
   // Function to find notifications by user ID
-  async findNotificationsByUser(userId: UserEntity): Promise<void> { 
-    try{
-      if(!userId){
+  async findNotificationsByUser(userId: UserEntity): Promise<void> {
+    try {
+      if (!userId) {
         throw new Error('User not found');
       }
       await this.notificationRepository.findOneByOrFail(userId);
-    }
-    catch(err){
+    } catch (err) {
       throw new Error(err);
     }
-   }
+  }
 }

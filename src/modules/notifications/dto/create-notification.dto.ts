9074c65ffb68bfdcdc9123be4ed5647ba1214573
pertinaz <manuel.agudelo1@utp.edu.co>
2024-07-19 08:55:03 -0500

    update notifications entity and create dto structure

--- src/modules/notifications/dto/create-notification.dto.ts
+++ src/modules/notifications/dto/create-notification.dto.ts
@@ -1,4 +1,11 @@
 // create a notification dto
 
 export class CreateNotificationDto {
+  emisorUser: string;
+  receptorUser: string; // FK
+  status: boolean;
+  action: Enumerator;
+  title: string;
+  description: string; // TXT
+  notificationDate: Date; // TIMESTAMP
 }

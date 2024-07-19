85f1344656a37739446d6c70c72b706f38ce2af7
JsGDmzB27 <josedago8@gmail.com>
2024-07-19 11:56:48 -0700

    userServices

--- src/modules/users/services/user.service.ts
+++ src/modules/users/services/user.service.ts
@@ -1,4 +1,4 @@
-/* import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm'; 
 import { UserEntity } from '../entities/user.entity';
@@ -8,56 +8,77 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(UserEntity)
-        private readonly userRepository: Repository<UserEntity>,
-        private readonly jwtService: JwtService,    
-        private readonly configService: ConfigService
-    ) {}
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+    /* private readonly jwtService: JwtService,    
+    private readonly configService: ConfigService */
+  ) {}
 
-    // Here is the services of user 
-
-    // first create user
-        // IN this service i need a function to create a user
-    // the name is createUser and the parameter is UserDto
-
-    async createUser(user: UserDto) {
+  async createUser(userDto: UserDto) {
+    try {
+      const user = this.userRepository.create(userDto);
+      return await this.userRepository.save(user);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
     // second login user
     // the name is loginUser and the parameter is UserDto
 
-    async loginUser(user: UserDto) {
+/*   async loginUser(userDto: UserDto) {
+    try {
+      const 
+    } catch (error) {
+      console.log(error);
     }
+  } */
 
     // logaut user
     // the name is logoutUser and the parameter is UserDto
 
-    async logoutUser(user: UserDto) {
-    }
+/*   async logoutUser(userDto: UserDto) {
+  } */
 
     // get user 
     // the name is getUser and the parameter is UserDto
 
-    async getUser(user: UserDto) {
+  async getUser(userId:string) {
+    try {
+      return await this.userRepository.findOne({ where: { id: userId } })
+    } catch (error) {
+      console.log(error)
     }
+  }
+
+  async getUsers (): Promise<UserEntity[]> {
+    try {
+      return await this.userRepository.find()
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
     // modify user
     // the name is modifyUser and the parameter is UserDto
 
-    async modifyUser(user: UserDto) {
+  async modifyUser(userDto: UserDto) {
+    try {
+      const user 
+    } catch (error) {
+      console.log(error)
     }
+  }
 
     // delete user
     // the name is deleteUser and the parameter is UserDto
 
-    async deleteUser(user: UserDto) {
-    }
+  async deleteUser(userDto: UserDto) {
+  }
 
     // modify the state of a darkMode 
 
-    async modifyDarkMode(user: UserDto) {
-    }
-}
-
- */
+  async modifyDarkMode(userDto: UserDto) {
+  }
+};

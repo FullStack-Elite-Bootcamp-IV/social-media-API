ffd1de9610c9e0e5d02175a66be8ed3efdbad248
JuanF-Cano <juan.cano5@utp.edu.co>
2024-07-18 20:53:56 -0500

    chat and user entity

---
+++ src/modules/chats/entities/chat.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { UserEntity } from '../../users/entities/user.entity';
+
+@Entity('Chats')
+export class ChatEntity {
+    @PrimaryColumn({ type: 'varchar', length: 100 })
+    id: string;
+
+    @Column({ type: 'varchar', length: 100 })
+    user1Id: string;
+
+    @Column({ type: 'varchar', length: 100 })
+    user2Id: string;
+
+    @Column({ type: 'timestamp', nullable: true })
+    lastMessage?: Date;
+
+    @ManyToOne(() => UserEntity, user => user.id)
+    @JoinColumn({ name: 'user1Id' })
+    user1: UserEntity;
+
+    @ManyToOne(() => UserEntity, user => user.id)
+    @JoinColumn({ name: 'user2Id' })
+    user2: UserEntity;
+}
--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,7 +1,58 @@
-// here to build the user entity
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
-
-
-@Entity()
+@Entity('User')
 export class UserEntity {
+    @PrimaryColumn({ type: 'varchar', length: 100 })
+    id: string;
+
+    @Column({ type: 'varchar', length: 100, unique: true })
+    username: string;
+
+    @Column({ type: 'varchar', length: 100, unique: true })
+    email: string;
+
+    @Column({ type: 'varchar', length: 255 })
+    password: string;
+
+    @Column({ type: 'varchar', length: 200 })
+    fullName: string;
+
+    @Column({ type: 'int', nullable: true })
+    age?: number;
+
+    @Column({ type: 'varchar', length: 50, nullable: true })
+    gender?: string;
+
+    @Column({ type: 'varchar', length: 255, nullable: true })
+    profileImage?: string;
+
+    @Column({ type: 'varchar', length: 255, nullable: true })
+    coverImage?: string;
+
+    @Column({ type: 'varchar', length: 500, nullable: true })
+    description?: string;
+
+    @Column({ type: 'varchar', length: 100, nullable: true })
+    college?: string;
+
+    @Column({ type: 'varchar', length: 100, nullable: true })
+    workPlace?: string;
+
+    @Column({ type: 'varchar', length: 100, nullable: true })
+    location?: string;
+
+    @Column({ type: 'varchar', length: 255, nullable: true })
+    personalWebSite?: string;
+
+    @Column({ type: 'boolean', default: false })
+    darkMode: boolean;
+
+    @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    creationDate: Date;
+
+    @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+    updateDate: Date;
+
+    @Column({ type: 'timestamp', nullable: true })
+    lastLoginDate?: Date;
 }

a0be1c0c584c2c25e51e32e2e3d2d5e139b315fb
JuanF-Cano <juan.cano5@utp.edu.co>
2024-07-18 20:58:06 -0500

    correcion espacios

--- src/modules/chats/entities/chat.entity.ts
+++ src/modules/chats/entities/chat.entity.ts
@@ -3,23 +3,23 @@ import { UserEntity } from '../../users/entities/user.entity';
 
 @Entity('Chats')
 export class ChatEntity {
-    @PrimaryColumn({ type: 'varchar', length: 100 })
-    id: string;
+  @PrimaryColumn({ type: 'varchar', length: 100 })
+  id: string;
 
-    @Column({ type: 'varchar', length: 100 })
-    user1Id: string;
+  @Column({ type: 'varchar', length: 100 })
+  user1Id: string;
 
-    @Column({ type: 'varchar', length: 100 })
-    user2Id: string;
+  @Column({ type: 'varchar', length: 100 })
+  user2Id: string;
 
-    @Column({ type: 'timestamp', nullable: true })
-    lastMessage?: Date;
+  @Column({ type: 'timestamp', nullable: true })
+  lastMessage?: Date;
 
-    @ManyToOne(() => UserEntity, user => user.id)
-    @JoinColumn({ name: 'user1Id' })
-    user1: UserEntity;
+  @ManyToOne(() => UserEntity, user => user.id)
+  @JoinColumn({ name: 'user1Id' })
+  user1: UserEntity;
 
-    @ManyToOne(() => UserEntity, user => user.id)
-    @JoinColumn({ name: 'user2Id' })
-    user2: UserEntity;
+  @ManyToOne(() => UserEntity, user => user.id)
+  @JoinColumn({ name: 'user2Id' })
+  user2: UserEntity;
 }
--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -2,57 +2,57 @@ import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } fro
 
 @Entity('User')
 export class UserEntity {
-    @PrimaryColumn({ type: 'varchar', length: 100 })
-    id: string;
+  @PrimaryColumn({ type: 'varchar', length: 100 })
+  id: string;
 
-    @Column({ type: 'varchar', length: 100, unique: true })
-    username: string;
+  @Column({ type: 'varchar', length: 100, unique: true })
+  username: string;
 
-    @Column({ type: 'varchar', length: 100, unique: true })
-    email: string;
+  @Column({ type: 'varchar', length: 100, unique: true })
+  email: string;
 
-    @Column({ type: 'varchar', length: 255 })
-    password: string;
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
 
-    @Column({ type: 'varchar', length: 200 })
-    fullName: string;
+  @Column({ type: 'varchar', length: 200 })
+  fullName: string;
 
-    @Column({ type: 'int', nullable: true })
-    age?: number;
+  @Column({ type: 'int', nullable: true })
+  age?: number;
 
-    @Column({ type: 'varchar', length: 50, nullable: true })
-    gender?: string;
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  gender?: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: true })
-    profileImage?: string;
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  profileImage?: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: true })
-    coverImage?: string;
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  coverImage?: string;
 
-    @Column({ type: 'varchar', length: 500, nullable: true })
-    description?: string;
+  @Column({ type: 'varchar', length: 500, nullable: true })
+  description?: string;
 
-    @Column({ type: 'varchar', length: 100, nullable: true })
-    college?: string;
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  college?: string;
 
-    @Column({ type: 'varchar', length: 100, nullable: true })
-    workPlace?: string;
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  workPlace?: string;
 
-    @Column({ type: 'varchar', length: 100, nullable: true })
-    location?: string;
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  location?: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: true })
-    personalWebSite?: string;
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  personalWebSite?: string;
 
-    @Column({ type: 'boolean', default: false })
-    darkMode: boolean;
+  @Column({ type: 'boolean', default: false })
+  darkMode: boolean;
 
-    @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-    creationDate: Date;
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  creationDate: Date;
 
-    @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
-    updateDate: Date;
+  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  updateDate: Date;
 
-    @Column({ type: 'timestamp', nullable: true })
-    lastLoginDate?: Date;
+  @Column({ type: 'timestamp', nullable: true })
+  lastLoginDate?: Date;
 }

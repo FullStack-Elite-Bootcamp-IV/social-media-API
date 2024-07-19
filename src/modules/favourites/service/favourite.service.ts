519152c825c36442e39bbe44b507cafde385e912
AGBR121 <angel.burbano@utp.edu.co>
2024-07-19 09:35:34 -0500

    Revisar comentarios y actualizacion de favouriteService

--- src/modules/favourites/service/favourite.service.ts
+++ src/modules/favourites/service/favourite.service.ts
@@ -10,26 +10,25 @@ export class FavouritesService {
         @InjectRepository(FavouritesEntity)
         private readonly favouritesRepository: Repository<FavouritesEntity>
     ) {}
-
-    // Here is the services of user 
-
     // first add favourite
         // IN this service i need a function to add a post in favourite
     // the name is addFavourite and the parameter is FavouritesDto
 
-    async addFavourites(favourite: FavouritesDto) {
+    async addFavourites(favourite: FavouritesDto):Promise<FavouritesEntity> {
+        return;
     }
 
     // second delete a Favourite post
     // the name is deleteFavourite and the parameter is FavouritesDto
 
-    async deleteFavourite(favourite: FavouritesDto) {
+    async deleteFavourite(favourite: FavouritesDto):Promise<FavouritesEntity>{
+        return;
     }
 
     // third get a favourites Post
     // the name is getFavourites and the parameter is FavouritesDto
 
-    async getFavourites (favourite: FavouritesDto) {
+    async getFavourites () {
     }
 
 }

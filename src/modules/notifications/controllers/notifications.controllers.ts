1567a86e03c8f1bfdd2bc26a4fc7494d6b8c7a1c
pertinaz <manuel.agudelo1@utp.edu.co>
2024-07-19 11:58:30 -0500

    add exceptions notifications service

--- src/modules/notifications/controllers/notifications.controllers.ts
+++ src/modules/notifications/controllers/notifications.controllers.ts
@@ -2,17 +2,18 @@ import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common'
 import { NotificationsService } from '../services/notifications.service';
 import { CreateNotificationDto } from '../dto/create-notification.dto';
 import { NotificationEntity } from '../entities/notification.entity';
+import { UserEntity } from 'src/modules/users/entities/user.entity';
 
 @Controller('notifications')
 export class NotificationsController {
   constructor(private notificationsService: NotificationsService) {}
 
-  //  @Post()
-  // createNotification(@Body() createNotificationDto: CreateNotificationDto): Promise<NotificationEntity> {return this.notificationsService.createNotification();}
+  @Post()
+  createNotification(@Body() createNotificationDto: CreateNotificationDto): Promise<NotificationEntity> {return this.notificationsService.createNotification(createNotificationDto);}
 
-  // @Delete(':id')
-  // deleteNotification(@Param('id') notificationId: string): Promise<void> { ... }
+  @Delete(':id')
+  deleteNotification(@Param('id') notificationId: string): Promise<void> { return this.notificationsService.deleteNotification(notificationId);}
 
-  // @Get('user/:userId')
-  // findNotificationsByUser(@Param('userId') userId: string): Promise<NotificationEntity[]> { ... }
+  @Get('user/:userId')
+  findNotificationsByUser(@Param('userId') userId: UserEntity): Promise<void> { return this.notificationsService.findNotificationsByUser(userId) }
 }
--- src/modules/notifications/services/notifications.service.ts
+++ src/modules/notifications/services/notifications.service.ts
@@ -6,7 +6,6 @@ import { CreateNotificationDto } from '../dto/create-notification.dto';
 import { NotificationEntity } from '../entities/notification.entity';
 import { UserEntity } from 'src/modules/users/entities/user.entity';
 
-
 @Injectable()
 export class NotificationsService {
   constructor(
@@ -16,14 +15,42 @@ export class NotificationsService {
 
   // Function to create a new notification
   async createNotification(createNotificationDto: CreateNotificationDto): Promise<NotificationEntity> {
-    const notification = this.notificationRepository.create(createNotificationDto);
-    await this.notificationRepository.save(notification);
-    return notification;
-   }
+    try{
+      const {emisorUser, receptorUser} = createNotificationDto
+      if(!emisorUser || !receptorUser){
+        throw new Error('Emisor user or receptor user are not valid');
+      }
+      const notification = this.notificationRepository.create(createNotificationDto);
+      await this.notificationRepository.save(notification);
+      return notification;
+    }catch(err){
+      throw new Error(err);
+    }
+  }
 
   // Function to delete a notification by ID
-  async deleteNotification(notificationId: string): Promise<void> { await this.notificationRepository.delete(notificationId) }
+  async deleteNotification(notificationId: string): Promise<void> { 
+    try{
+      if(!notificationId){
+        throw new Error('Notification inexistent');
+      }
+      await this.notificationRepository.delete(notificationId);
+    }
+    catch(err){
+      throw new Error(err);
+    }
+   }
 
   // Function to find notifications by user ID
-  async findNotificationsByUser(userId: UserEntity): Promise<void> { await this.notificationRepository.findOneByOrFail(userId) }
+  async findNotificationsByUser(userId: UserEntity): Promise<void> { 
+    try{
+      if(!userId){
+        throw new Error('User not found');
+      }
+      await this.notificationRepository.findOneByOrFail(userId);
+    }
+    catch(err){
+      throw new Error(err);
+    }
+   }
 }

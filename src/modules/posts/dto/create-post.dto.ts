5237fba046870bd4882432814af5357adb77a4e2
JsGDmzB27 <josedago8@gmail.com>
2024-07-19 09:15:15 -0700

    user DTO

--- src/modules/posts/dto/create-post.dto.ts
+++ src/modules/posts/dto/create-post.dto.ts
@@ -1,5 +1,43 @@
 // here you must to create a post dto
+import {
+  IsString,
+  IsBoolean,
+  IsDate,
+  IsNumber,
+  IsNotEmpty,
+} from 'class-validator';
+
 export class CreatePostDto {
-    // here you must to create a post dto whit the properties based on the data base
-  }
-  
+
+  @IsNotEmpty()
+  @IsString()
+  userId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  media: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  isPublic: boolean;
+
+  @IsNotEmpty()
+  @IsDate()
+  publicationDate: Date;
+
+  @IsNotEmpty()
+  @IsDate()
+  updateDate: Date;
+
+  @IsNotEmpty()
+  @IsNumber()
+  likes: number;
+}
--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,8 +1,53 @@
 // here you must tyo create the user dto
 
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsNumber, IsPositive, IsString, MinLength } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { UserEntity } from '../entities/user.entity';
 
 export class UserDto extends PartialType(UserEntity) {
+
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @IsString()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  fullName: string;
+
+  @IsNumber()
+  @IsPositive()
+  age?: number;
+
+  @IsString()
+  gender?: string;
+
+  @IsString()
+  profileImage?: string;
+
+  @IsString()
+  coverImage?: string;
+
+  @IsString()
+  description?: string;
+
+  @IsString()
+  college?: string;
+
+  @IsString()
+  workPlace?: string;
+
+  @IsString()
+  location?: string;
+
+  @IsString()
+  personalWebSite?: string;
 }

838ecc3a599e388b2563dd8b8fc95d9d77b7af01
FrankiNarvaez <frannki03@gmail.com>
2024-07-20 18:34:49 -0500

    Added like methods

--- src/modules/likes/dto/create-like.dto.ts
+++ src/modules/likes/dto/create-like.dto.ts
@@ -3,24 +3,13 @@ import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 import { PostEntity } from '../../posts/entities/post.entity';
 import { UserEntity } from '../../users/entities/user.entity';
 export class CreateLikeDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  postId: string;
 
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
-  id: string;
-
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-  postId: PostEntity;
-
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-  userId: UserEntity;
-
-  @ApiProperty()
-  @IsDate()
-  @IsNotEmpty()
-  creationDate: Date;
+  userId: string;
 }

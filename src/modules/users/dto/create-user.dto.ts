9b511af2cc68cac211ac3ec91a08caf2b0a9399c
VegaSyd <nicolas.vega@utp.edu.co>
2024-07-21 00:02:41 -0500

    logoutUpdateDTO

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,6 +1,6 @@
 // here you must tyo create the user dto
 
-import { IsDate, IsEmail, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString, MinLength } from 'class-validator';
+import { IsDate, IsDateString, IsEmail, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString, MinLength } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { UserEntity } from '../entities/user.entity';
 import { Gender } from '../entities/user.entity';
@@ -67,5 +67,6 @@ export class UserDto extends PartialType(UserEntity) {
   personalWebSite?: string;
 
   @ApiProperty()
+  @IsDateString()
   lastLogoutDate?: string;
 }
